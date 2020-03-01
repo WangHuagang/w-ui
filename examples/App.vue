e012e9bdd6e025faef15a048a3a6b5604be37f81
wanghuagang <1154438272@qq.com>
2020-03-01 14:19:51 +0800

    init

---
+++ examples/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <w-button type='primary'>按钮</w-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import WUI from '../packages/index'
 
 Vue.config.productionTip = false
+Vue.use(WUI)
 
 new Vue({
   render: h => h(App),
